05564f6c71796e3dcba2842632660acfa6714bc2
Fabricio Souza <fabriciosouza@santos.sp.gov.br>
2019-07-17 14:16:53 -0300

    add template bootstrap 4 e css no main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-import Vue from "vue"
-import App from "./App.vue"
-import router from "./router"
-import store from "./store"
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-library.add(faUserSecret)
+// imports para o template sb admin 2 bootstrap 4
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "@/template/css/fonts.googleapis.nunito.css";
+import "@/template/css/sb-admin-2.css";
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faUserSecret);
 
-Vue.config.productionTip = false
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app")
+}).$mount("#app");
