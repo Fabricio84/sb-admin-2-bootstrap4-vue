f6bab847e8a39c635c3b9f8ba3e8b4d05915205e
Fabricio Souza <fabriciosouza@santos.sp.gov.br>
2019-07-26 17:03:58 -0300

    add de css e scripts no layout

--- src/main.js
+++ src/main.js
@@ -10,9 +10,21 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import "@/template/css/fonts.googleapis.nunito.css";
 import "@/template/css/sb-admin-2.css";
 
+import $ from "jquery";
+import { Layout } from "bootstrap-vue/es/components";
+import "jquery-easing";
+import "@/template/js/sb-admin-2.js";
+import VueCharts from 'vue-chartjs'
+
+import "@/template/js/demo/chart-area-demo.js"
+import "@/template/js/demo/chart-pie-demo.js"
+
 library.add(faUserSecret);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.use(Layout);
+Vue.use(VueCharts);
 
 Vue.config.productionTip = false;
 
